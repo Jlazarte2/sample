e4e962222daaee946cf576ce82a41ea936ff4509
jlazarte2 <jlazarte2@dxc.com>
2020-08-05 15:38:35 +0800

    a

--- php/connections.php
+++ php/connections.php
@@ -1,11 +1,10 @@
 <?php
-
+    //$servername = "usyd1.czgixbvgrqdc.us-east-2.rds.amazonaws.com";
+    //$username = "admin";
+    //$password = "usydadmin";
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $servername = "usyd1.czgixbvgrqdc.us-east-2.rds.amazonaws.com";
-    $username = "admin";
-    $password = "usydadmin";
     $databasename = "ssr_database";
 
     $connections = mysqli_connect($servername, $username, $password, $databasename);
